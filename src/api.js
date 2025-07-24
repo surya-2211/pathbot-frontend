bb7005d60a066b13b4b21e7b3ac9784660b7df8c
surya-2211 <nagasurya2211@gmail.com>
2025-07-24 19:46:21 +0530

    feat env file added

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_BACKEND_URL + '/api',
 });
 
 API.interceptors.request.use((req) => {
